46407bb6a571bfa1ff07e1ec84b3b2082890d7e5
MrBlueSkye <32515298+MrBlueSkye@users.noreply.github.com>
2018-04-13 12:23:43 +0100

    Update annoying_bot.py

--- annoying_bot.py
+++ annoying_bot.py
@@ -4,9 +4,8 @@ import praw
 import time
 import random
 import markovify
-import learningcomments as lc
 
-reddit = praw.Reddit('SkyeBot')
+reddit = praw.Reddit('MY_BOT_USERNAME')
 subreddit = reddit.subreddit("All")
 comments = subreddit.stream.comments()
 signalsentence="These are words to look for :)"
@@ -23,54 +22,63 @@ chattingshit = [
 	'Let us be friends!',
 	'You are a nice person!',
 	'Have a great day!',
-] # FIXME: Skylar's bot can be mean sometimes...
-
-#chattingshit.append(lc.learned())
+]
 
 # []
 
 
 # Get raw text as string.
-with open("/home/skylar/Scripts/RedditBot/corpus.txt") as f:
+with open("path/to/corpus.txt") as f:
     text = f.read()
 
 # Build the model.
-text_model = markovify.Text(text)
+text_model = markovify.NewlineText(text)
 
-# Print five randomly-generated sentences
-for i in range(5):
-    shaekeitUp = text_model.make_sentence()
-    chattingshit.append(shaekeitUp)
-    print shaekeitUp
+try:
+    for comment in comments:
+        author = comment.author # Fetch author
+        text = comment.body # Fetch body
+        if keyword in text.lower():
+            if author != "MY_BOT_USERNAME":
+                    # Generate a message
+                    for i in range(3):
+                        shaekeitUp = text_model.make_sentence()
+                        chattingshit.append(shaekeitUp)
+                        print shaekeitUp
+                        upboats = comment.score
+                        print upboats
+                        signalsentence=random.choice(text.split())
+    #               chattingshit.append(text) # moved after comment submission...picking uncommon random word often results to replying to user with their own comment
+    #               goodcomment =                
+                        postingshit = random.choice(chattingshit)
+                        message = postingshit.format(author)
+                        comment.reply(message) # Send message
+                        chattingshit.append(text) # previously (text,upboats)...maybe didn't work
+                        print message
+                        time.sleep(20)
+    #		print("Title: ", submission.title)
+    #		print("Text: ", submission.selftext)
+    #		print("Score: ", submission.score)
+    #		print("---------------------------------\n \n")
+    #
+    #		quit()
 
-for comment in comments:
-    author = comment.author # Fetch author
-    text = comment.body # Fetch body
-    if keyword in text.lower():
-	if author != "SkyeBot":
-        	# Generate a message
-                upboats = comment.score
-                print upboats
-                signalsentence=random.choice(text.split())
-#               chattingshit.append(text) # moved after comment submission...picking uncommon random word often results to replying to user with their own comment
-#               goodcomment =                
-                postingshit = random.choice(chattingshit)
-                message = postingshit.format(author)
-	        comment.reply(message) # Send message
-                chattingshit.append(text) # previously (text,upboats)...maybe didn't work
-                print message
-                time.sleep(1)
-#		print("Title: ", submission.title)
-#		print("Text: ", submission.selftext)
-#		print("Score: ", submission.score)
-#		print("---------------------------------\n \n")
-#
-#		quit()
 
+    for submission in subreddit.hot(limit=10):
+        print("Title: ", submission.title)
+        print("Text: ", submission.selftext)
+        print("Score: ", submission.score)
+        print("---------------------------------\n")
 
-for submission in subreddit.hot(limit=10):
-    print("Title: ", submission.title)
-    print("Text: ", submission.selftext)
-    print("Score: ", submission.score)
-    print("---------------------------------\n")
-                
+except KeyboardInterrupt:
+    print("Shutting down.")
+    quit()
+except praw.errors.HTTPException as e:
+    exc = e._raw
+    print("Some thing bad happened! HTTPError", exc.status_code)
+    if exc.status_code == 503:
+        print("Let's wait til reddit comes back! Sleeping 60 seconds.")
+        time.sleep(60)
+except Exception as e:
+    print("Some thing bad happened!", e)
+    traceback.print_exc()                
